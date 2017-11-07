7bd7814d3eff8e3d1b7dbf23421f2059eb609870
humbertojacobm <humbertojacobm@gmail.com>
2017-11-07 00:34:34 -0500

    f

---
+++ Parte01Problema01/ChangeString.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte01Problema01
+{
+    public class ChangeString
+    {
+        public string build(string prmBase)
+        {
+            string Result = "";
+            string AlphabetMinus = "abcdefghijklmnñopqrstuvwxyz";
+            char[] alphabetMinus = AlphabetMinus.ToCharArray();
+            char[] alphabetMayus = AlphabetMinus.ToUpper().ToCharArray();
+            const int FirstPosition = 0;
+            const int LastPosition = 26;
+            int CurrentPositionOfLetter = 0;
+            char[] word = prmBase.ToCharArray();
+
+            foreach (char letter in word)
+            {
+                char CurrentChar = letter;
+
+                if (char.IsLetter(CurrentChar))
+                {
+
+                    CurrentPositionOfLetter = 0;
+
+                    if (char.IsUpper(CurrentChar))
+                    {
+                        CurrentPositionOfLetter = alphabetMayus.ToList().FindIndex(x => x == letter);
+                    }
+                    else
+                    {
+                        CurrentPositionOfLetter = alphabetMinus.ToList().FindIndex(x => x == letter);
+                    }                    
+
+                    if (CurrentPositionOfLetter == LastPosition)
+                    {
+                        CurrentPositionOfLetter = FirstPosition;
+                    }
+                    else
+                    {
+                        CurrentPositionOfLetter++;
+                    }
+
+                    if (char.IsUpper(CurrentChar))
+                    {
+                        CurrentChar = alphabetMayus.ToList()[CurrentPositionOfLetter];
+                    }
+                    else
+                    {
+                        CurrentChar = alphabetMinus.ToList()[CurrentPositionOfLetter];
+                    }
+                        
+                }
+               
+                Result += CurrentChar;
+
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ Parte01Problema01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte01Problema01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("INSTRUCCIONES");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("01. Siempre presione ENTER para avanzar");
+            Console.WriteLine("02. Recuerda que para salir del programa escriba SALIR respetando las mayúsculas");
+            Console.WriteLine("03. Escriba alguna palabra que desee transformar, presion ENTER");            
+
+            string word= Console.ReadLine();
+            string ExitWord = "SALIR";
+            ChangeString _ChangeString = new ChangeString();
+            string Result = "";
+
+            while (word != ExitWord)
+            {                
+
+                Result = "";
+                
+                Result = _ChangeString.build(word);
+
+                Console.WriteLine(Result);
+                Console.WriteLine("-------------------------------------");
+                word = Console.ReadLine();
+            }
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("GRACIAS POR UTILIZAR EL PROGRAMA, para salir presione alguna tecla");
+            Console.ReadKey();
+
+            
+        }
+    }
+}
